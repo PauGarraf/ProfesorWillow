84deb1184f6704617d6e71309f5fbf19e75c05a0
Pau Martínez <paugarraf@gmail.com>
2018-04-19 14:22:36 +0200

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=profwillow-Web-Deploy.pubxml -p:Password=pmqdREGYj8ZkZhMYCsj8md6FhvnTkswxSLoeTXSvPR1Bbk1whpihK84h9ABC
+
